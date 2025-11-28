fc56b64c5aa91d88e495468a6ca51bdeb3af3465
Alexandre Nascimento <alexandre.nascimento@dataprev.gov.br>
2025-11-28 11:21:46 -0300

    refatorando para separar consultas e renderização

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
-import { clearChart } from './clearChart';
 import { Crash } from './crash';
-
-
-function callbacks(data, pickupLocationsData, taxi, zonesData) {
-    
-    const clearBtn = document.querySelector('#clearBtn');
-
-    if (!loadBtn || !clearBtn) {
-        return;
-    }
-
-    clearBtn.addEventListener('click', async () => {
-        clearChart();
-    });
-}
+import { acidentesPorDestrito } from './analises/acidentesPorDestrito';
+import { acidentesPorHorario } from './analises/acidentesPorHorario';
 
 window.onload = async () => {
     const crash = new Crash("Motor_Vehicle_Collisions_-_Crashes.csv");
@@ -24,50 +11,7 @@ window.onload = async () => {
     window.crash = crash;
     window.q = (sql) => crash.query(sql);
 
-    const sql = `
-        SELECT borough,
-        COUNT(*) AS total
-        FROM crashes
-        WHERE borough IS NOT NULL AND borough != ''
-        GROUP BY borough
-        ORDER BY total DESC;
-    `;
+    document.getElementById("btn-destrito").onclick = () => acidentesPorDestrito(crash);
+    document.getElementById("btn-horario").onclick = () => acidentesPorHorario(crash);
 
-    const data = await crash.query(sql);
-    console.log("Acidentes por borough:", data);
-
-    renderTable(data);
-}
-
-
-function renderTable(data) {
-    const container = document.getElementById('result-table');
-    
-    if (!data || data.length === 0) {
-        container.innerHTML = "<p>Nenhum dado encontrado.</p>";
-        return;
-    }
-
-    // Cria a tabela
-    let html = "<table border='1' cellpadding='5'><thead><tr>";
-
-    // Cabeçalhos
-    Object.keys(data[0]).forEach(key => {
-        html += `<th>${key}</th>`;
-    });
-
-    html += "</tr></thead><tbody>";
-
-    // Dados
-    data.forEach(row => {
-        html += "<tr>";
-        Object.values(row).forEach(value => {
-            html += `<td>${value}</td>`;
-        });
-        html += "</tr>";
-    });
-
-    html += "</tbody></table>";
-
-    container.innerHTML = html;
 }
---
+++ src/renderTable.js
@@ -0,0 +1,28 @@
+export function renderTable(data) {
+    const container = document.getElementById('result-table');
+    
+    if (!data || data.length === 0) {
+        container.innerHTML = "<p>Nenhum dado encontrado.</p>";
+        return;
+    }
+
+    let html = "<table border='1' cellpadding='5'><thead><tr>";
+
+    Object.keys(data[0]).forEach(key => {
+        html += `<th>${key}</th>`;
+    });
+
+    html += "</tr></thead><tbody>";
+
+    data.forEach(row => {
+        html += "<tr>";
+        Object.values(row).forEach(value => {
+            html += `<td>${value}</td>`;
+        });
+        html += "</tr>";
+    });
+
+    html += "</tbody></table>";
+
+    container.innerHTML = html;
+}

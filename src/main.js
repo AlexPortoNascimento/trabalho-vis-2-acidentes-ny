01f242f39533cb456549dae1710bf11a58f4ea9f
Alexandre Nascimento <alexandreporto@id.uff.br>
2025-11-30 12:14:11 -0300

    Refatorando querys

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { Crash } from './crash';
-import { acidentesPorDistrito } from './analises/acidentesPorDistrito.js';
 import { setupTabs } from './tabs.js/';
-import { acidentesPorHorarioQuery } from './analises/acidentesPorHorarioQuery.js';
+
 
 window.onload = async () => {
     const crash = new Crash("Motor_Vehicle_Collisions_-_Crashes.csv");
@@ -11,10 +10,6 @@ window.onload = async () => {
     window.crash = crash;
     window.q = (sql) => crash.query(sql);
 
-    setupTabs(crash)
-
-    await acidentesPorDistrito(crash, "analise-matheus");
-    await acidentesPorHorarioQuery(crash, "analise-alexandre");
-
+    setupTabs(crash);
 
 }
